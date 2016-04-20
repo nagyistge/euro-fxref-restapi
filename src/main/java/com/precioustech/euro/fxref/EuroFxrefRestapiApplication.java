41e955698bae0456f0c9524eb01b1b87d717a518
shekhar varshney <shekhar.varshney@gmail.com>
2016-04-20 14:47:24 +0200

    Create skeleton spring boot app

---
+++ src/main/java/com/precioustech/euro/fxref/EuroFxrefRestapiApplication.java
@@ -0,0 +1,12 @@
+package com.precioustech.euro.fxref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroFxrefRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroFxrefRestapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/precioustech/euro/fxref/EuroFxrefRestapiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.precioustech.euro.fxref;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EuroFxrefRestapiApplication.class)
+@WebAppConfiguration
+public class EuroFxrefRestapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
