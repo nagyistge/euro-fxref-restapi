060f3452f9c970fda93a68bd0ef2c6066ff37dff
shekhar varshney <shekhar.varshney@gmail.com>
2016-04-20 16:00:27 +0200

    add parser interface and skeleton xml implementation

---
+++ src/main/java/com/precioustech/euro/fxref/parser/EcbXmlRateParser.java
@@ -0,0 +1,15 @@
+package com.precioustech.euro.fxref.parser;
+
+import java.text.ParseException;
+import java.util.List;
+
+import com.precioustech.euro.fxref.entities.EuroRate;
+
+public class EcbXmlRateParser implements RateSourceParser {
+
+	@Override
+	public List<EuroRate> parse(String payLoad) throws ParseException {
+		return null;
+	}
+
+}
---
+++ src/main/java/com/precioustech/euro/fxref/parser/RateSourceParser.java
@@ -0,0 +1,11 @@
+package com.precioustech.euro.fxref.parser;
+
+import java.text.ParseException;
+import java.util.List;
+
+import com.precioustech.euro.fxref.entities.EuroRate;
+
+public interface RateSourceParser {
+	
+	List<EuroRate> parse(String payLoad) throws ParseException;
+}
