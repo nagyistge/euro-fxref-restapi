19fdc9d30e8c26270dc9c543b74811feecd5fc9a
shekhar varshney <shekhar.varshney@gmail.com>
2016-04-21 08:31:56 +0200

    remove commented out code

--- src/main/java/com/precioustech/euro/fxref/EuroFxrefRestapiApplication.java
+++ src/main/java/com/precioustech/euro/fxref/EuroFxrefRestapiApplication.java
@@ -9,18 +9,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableScheduling
-//@Configuration
 public class EuroFxrefRestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EuroFxrefRestapiApplication.class, args);
 	}
-	
-//	@Bean
-//	public ObjectMapper objectMapper() {
-//		ObjectMapper mapper = new ObjectMapper();
-//		mapper.registerModule(new JodaModule());
-//		return mapper;
-//	}
 	
 }
